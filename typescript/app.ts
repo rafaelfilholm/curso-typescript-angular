4a0393e6d9aac72a149b904aa7fcfdad38fe9d70
Rafael Filho <rafaelfilholm@gmail.com>
2018-06-07 22:46:20 -0300

    typescript: arquivo tsconfig.json e inferencia de tipos

---
+++ typescript/app.ts
@@ -0,0 +1,5 @@
+let message: string = "You're Welcome!";
+let myAge: number = 18;
+let learningTypescript: boolean = true;
+let fruitsList: Array<string> = ["Apple", "Banana", "Orange"];
+let numbersList: number[] = [8, 10, 12, 1];
