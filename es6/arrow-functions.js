1a1ecbb22693102cd987918b8b3991f2c9c35ee5
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-06-13 22:07:39 -0300

    es6: created arrow-functions and variables with let

---
+++ es6/arrow-functions.js
@@ -0,0 +1,10 @@
+function sum1(firstValue, secondValue) { 
+	
+	return firstValue + secondValue;
+}
+
+
+const sum2 = (firstValue, secondValue) => firstValue + secondValue;
+
+console.log(sum1(10,20));
+console.log(sum2(10,20));
---
+++ es6/variables.js
@@ -0,0 +1,14 @@
+var message = "Hello JS!";
+
+const PRICE = 15.50;
+
+function showMessage(){
+	let message = "New message";
+	console.log(message);
+}
+
+
+console.log(message);
+console.log(PRICE);
+
+showMessage();
