bb4388f99c860be492e267aa476d00a481565cc9
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-12 11:56:15 -0300

    feat(project.app1): Finalizado front-end do dashboard do app1

--- projects/app1/src/app/app.module.ts
+++ projects/app1/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AttemptsComponent } from './components/attempts/attempts.component';
+import { ProgressComponent } from './components/progress/progress.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    AttemptsComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule
