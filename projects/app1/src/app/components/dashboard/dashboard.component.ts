8e9f5d605982a670142b42ea2b2b4fc1dd047746
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-12 23:52:22 -0300

    feat(projects.app1): Add one phrase dinamically and instruction on dashboard

--- projects/app1/src/app/components/dashboard/dashboard.component.ts
+++ projects/app1/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Phrase } from '../shared/phrase.model';
+import { PHRASES } from './phrase-mock';
+
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+	public phrases: Phrase[] = PHRASES;
+	public instruction: string = 'Traduza a seguinte frase:';
 
-  ngOnInit() {
-  }
+	constructor() { 
+		console.log(this.phrases)
+	}
+
+	ngOnInit() {
+	}
 
 }
