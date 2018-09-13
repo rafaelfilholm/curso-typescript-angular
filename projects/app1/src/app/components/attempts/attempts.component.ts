d6d8063c48b931cdbe58299a27bb1ce00c6184a1
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-12 23:34:34 -0300

    feat(projects.app1): Add hearts images

--- projects/app1/src/app/components/attempts/attempts.component.ts
+++ projects/app1/src/app/components/attempts/attempts.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-attempts',
-  templateUrl: './attempts.component.html',
-  styleUrls: ['./attempts.component.css']
+	selector: 'app-attempts',
+	templateUrl: './attempts.component.html',
+	styleUrls: ['./attempts.component.css']
 })
 export class AttemptsComponent implements OnInit {
 
-  constructor() { }
+	public empty_heart: string = 'assets/images/empty_heart.png';
+	public full_heart: string = 'assets/images/full_heart.png';
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() { }
 
 }
