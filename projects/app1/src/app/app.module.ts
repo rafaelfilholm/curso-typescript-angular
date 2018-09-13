46858ba3d8475e9eb2e9150541eebdb73c38ea51
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-13 00:43:01 -0300

    feat(projects.app1): Add FormsModule to app.module

--- projects/app1/src/app/app.module.ts
+++ projects/app1/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +18,8 @@ import { ProgressComponent } from './components/progress/progress.component';
     ProgressComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
