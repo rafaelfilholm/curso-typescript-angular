9139b3a9678f7eb593928894fa25e8a31f5ddd73
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-09-13 00:16:06 -0300

    fix(projects.app1): Add check of answer and increments of this

--- projects/app1/src/app/components/dashboard/dashboard.component.ts
+++ projects/app1/src/app/components/dashboard/dashboard.component.ts
@@ -12,12 +12,28 @@ export class DashboardComponent implements OnInit {
 
 	public phrases: Phrase[] = PHRASES;
 	public instruction: string = 'Traduza a seguinte frase:';
+	public answer: string = '';
+	public currentRound: number = 0;
 
-	constructor() { 
-		console.log(this.phrases)
-	}
+	constructor() { }
 
 	ngOnInit() {
 	}
 
+	public updateAnswer(event: Event): void {
+		this.answer = (<HTMLInputElement>event.target).value;
+	}
+
+	public checkAnswer(): void {
+
+		let currentPhrase = this.phrases[this.currentRound];
+
+		if(currentPhrase.phrasePtBr == this.answer)
+		{
+			alert('A tradução está correta!');
+			this.currentRound = this.currentRound + 1;
+		} else {
+			alert('A tradução está incorreta!');
+		}
+	}
 }
